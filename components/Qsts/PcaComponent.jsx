6ca212e427f358a476dceaf07f91237e8021f63f
RayaneA7 <ka_kebir@esi.dz>
2023-10-03 22:34:00 +0100

    fixed naming

---
+++ components/Qsts/PcaComponent.jsx
@@ -0,0 +1,22 @@
+import { Card, BarChart, Title, Text } from "@tremor/react";
+
+
+export default function PcaComponent({chartdata}) {
+  return (
+    <Card className="bg-gray2">
+      <Title>Ration variance entre les composantes</Title>
+      <BarChart
+        className="mt-4 h-80"
+        data={chartdata}
+        index="comp"
+        categories={["var"]}
+        colors={["indigo"] }
+        stack={false}
+        yAxisWidth={60}
+        valueFormatter={(number) =>
+          `$ ${Intl.NumberFormat("us").format(number).toString()}`
+        }
+      />
+    </Card>
+  );
+}
